c82cbd3db359e6987ea62d7bab2b0474c2f0b4a9
Maxime <max@desktop-mtbnc41.home>
2020-03-22 10:02:05 +0100

    add solver

--- solver_dir/main.c
+++ solver_dir/main.c
@@ -6,9 +6,27 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "include/lib.h"
+
+void init_struct(map_t *map)
+{
+    map->end = 0;
+    map->x = 0;
+    map->y = 0;
+    map->left = 0;
+    map->right = 0;
+    map->up = 0;
+    map->bot = 0;
+}
 
 int main(int ac, char **av)
 {
-    printf("hello\n");
+    map_t *map = malloc(sizeof(map_t));
+    char *file = open_file(av[1]);
+
+    str_to_arr(file, map);
+    init_struct(map);
+    solve(map);
     return (0);
 }
